32a20306ff4b48fa086958003ed0b976eed9919a
Van <142906115+VanAdcox@users.noreply.github.com>
2025-03-27 23:34:21 -0500

    start implementing dashboard functionality, msot active user, most active reader

--- Features/State/FileProcessingService.cs
+++ Features/State/FileProcessingService.cs
@@ -1,14 +1,32 @@
 using Microsoft.AspNetCore.Components.Forms;
 
-public class FileProccessingService {
-	private IBrowserFile? _rawFile;
+public class FileProcessingService {
 	public List<ReaderEvent>? ReaderEvents;
 
 	public event Action? OnDataProcessed;
 
 	public async Task ProcessFileAsync(IBrowserFile file) {
-		// TODO actually process file
-		_rawFile = file;
+		ReaderEvents = new List<ReaderEvent>();
+		await using var stream = file.OpenReadStream(long.MaxValue);
+		using var sr = new StreamReader(stream);
+		await sr.ReadLineAsync();
+
+		string? line;
+		while ((line = await sr.ReadLineAsync()) != null)
+		{
+			string[] values = line.Split(',');
+			if (values.Length == 6)
+			{
+				ReaderEvents.Add(new ReaderEvent(
+					DateTime.Parse(values[0].Trim('"')),
+					values[1].Trim('"'),
+					values[2].Trim('"'),
+					values[3].Trim('"'),
+					Convert.ToInt32(values[4].Trim('"')),
+					Convert.ToInt32(values[5].Trim('"'))
+				));
+			}
+		}
 		OnDataProcessed?.Invoke();
 	}
 }
