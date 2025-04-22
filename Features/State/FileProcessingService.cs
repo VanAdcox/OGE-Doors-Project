8e37dfaf456ac81dee73f8a3d8c1349ce6826249
fetch <142906115+VanAdcox@users.noreply.github.com>
2025-04-22 10:49:28 -0500

    filter out duplicate scans

--- Features/State/FileProcessingService.cs
+++ Features/State/FileProcessingService.cs
@@ -12,21 +12,29 @@ public class FileProcessingService {
 		await sr.ReadLineAsync();
 
 		string? line;
+		string lastLine = "";
+		int duplicateLines = 0;
 		while ((line = await sr.ReadLineAsync()) != null)
 		{
-			string[] values = line.Split(',');
-			if (values.Length == 6)
-			{
-				ReaderEvents.Add(new ReaderEvent(
-					DateTime.Parse(values[0].Trim('"')),
-					values[1].Trim('"'),
-					values[2].Trim('"'),
-					values[3].Trim('"'),
-					Convert.ToInt32(values[4].Trim('"')),
-					Convert.ToInt32(values[5].Trim('"'))
-				));
+			if(line != lastLine) {
+				string[] values = line.Split(',');
+				if (values.Length == 6)
+				{
+					ReaderEvents.Add(new ReaderEvent(
+						DateTime.Parse(values[0].Trim('"')),
+						values[1].Trim('"'),
+						values[2].Trim('"'),
+						values[3].Trim('"'),
+						Convert.ToInt32(values[4].Trim('"')),
+						Convert.ToInt32(values[5].Trim('"'))
+					));
+				}
+			} else {
+				duplicateLines++;
 			}
+			lastLine = line;
 		}
+		Console.WriteLine($"{duplicateLines} Duplicate Entries Found");
 		OnDataProcessed?.Invoke();
 	}
 }
